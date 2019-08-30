ed70022869bd33dfcf32b4a171f64ec25efe687e
Tomas Janousek <tomi@nomi.cz>
2019-08-30 20:29:25 +0200

    Enumerating inputs (try 2, conduits)

--- preprocessor/src/Main/Input.hs
+++ preprocessor/src/Main/Input.hs
@@ -1,22 +1,28 @@
 module Main.Input where
 
-import System.Directory (listDirectory, doesDirectoryExist, doesFileExist)
-import System.FilePath ((</>), isExtensionOf)
+import Conduit
+import System.Directory (doesDirectoryExist, doesFileExist)
+import System.FilePath (isExtensionOf)
 
-gpxInDirectory :: FilePath -> IO [FilePath]
-gpxInDirectory d = (map (d </>) . filter isGpx) <$> listDirectory d
-
-expandInputs :: [FilePath] -> IO [FilePath]
-expandInputs is = mconcat <$> mapM expandInput is
-
-expandInput :: FilePath -> IO [FilePath]
-expandInput i = do
-    isDir <- doesDirectoryExist i
-    isFile <- doesFileExist i
-    case (isDir, isFile) of
-        (True, _) -> gpxInDirectory i
-        (_, True) | isGpx i -> pure [i]
-        (_, _) -> error $ "bad input file/dir: " <> i
+type M = ResourceT IO
 
 isGpx :: FilePath -> Bool
 isGpx f = "gpx" `isExtensionOf` f || "gpx.gz" `isExtensionOf` f
+
+gpxInDirectoryC :: FilePath -> ConduitT a FilePath M ()
+gpxInDirectoryC d = sourceDirectoryDeep False d .| filterC isGpx
+
+expandInputC :: FilePath -> ConduitT a FilePath M ()
+expandInputC i = do
+    isDir <- liftIO $ doesDirectoryExist i
+    isFile <- liftIO $ doesFileExist i
+    case (isDir, isFile) of
+        (True, _) -> gpxInDirectoryC i
+        (_, True) | isGpx i -> yield i
+        (_, _) -> fail $ "bad input file/dir: " <> i
+
+expandInputsC :: ConduitT FilePath FilePath M ()
+expandInputsC = awaitForever expandInputC
+
+expandInputs :: [FilePath] -> IO [FilePath]
+expandInputs is = runConduitRes $ yieldMany is .| expandInputsC .| sinkList

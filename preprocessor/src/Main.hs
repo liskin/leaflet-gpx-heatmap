1226452d57fd6f3e70ce5d24e81527306565c16a
Tomas Janousek <tomi@nomi.cz>
2019-09-01 01:05:31 +0200

    Refactor

--- preprocessor/src/Main.hs
+++ preprocessor/src/Main.hs
@@ -1,4 +1,14 @@
 module Main (main) where
 
+import Conduit
+import System.Environment (getArgs)
+
+import Main.Input
+
 main :: IO ()
-main = pure ()
+main = do
+    args <- getArgs
+    parseInputs processData args
+
+processData :: ConduitT InputEvent () M ()
+processData = filterC (== EndTrkSeg) .| mapM_C (liftIO . print)
--- preprocessor/src/Main/Input.hs
+++ preprocessor/src/Main/Input.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main.Input where
+module Main.Input (InputEvent(..), M, parseInputs) where
 
 import Conduit
 import Control.Monad
 import Data.ByteString (ByteString)
 import Data.Conduit.Zlib (ungzip)
 import Data.Default
-import Data.XML.Types (Event)
 import System.Directory (doesDirectoryExist, doesFileExist)
 import System.FilePath (isExtensionOf)
 import Text.XML (Name(nameLocalName))
@@ -20,49 +19,47 @@ type M = ResourceT IO
 data InputEvent = EndTrkSeg | Point{ ptLat :: Double, ptLon :: Double }
     deriving (Show, Eq, Ord)
 
-isGpx :: FilePath -> Bool
-isGpx f = "gpx" `isExtensionOf` f || "gpx.gz" `isExtensionOf` f
-
-gpxInDirectoryC :: FilePath -> ConduitT a FilePath M ()
-gpxInDirectoryC d = sourceDirectoryDeep False d .| filterC isGpx
-
-processFileC :: ConduitT ByteString o M () -> FilePath -> ConduitT i o M ()
-processFileC c i
-    | "gpx" `isExtensionOf` i = sourceFile i .| c
-    | "gpx.gz" `isExtensionOf` i = sourceFile i .| ungzip .| c
-    | otherwise = fail $ "bad input file/dir: " <> i
-
-processInputC :: ConduitT ByteString o M () -> FilePath -> ConduitT i o M ()
-processInputC c i = do
-    isDir <- liftIO $ doesDirectoryExist i
-    isFile <- liftIO $ doesFileExist i
-    case (isDir, isFile) of
-        (True, _) -> gpxInDirectoryC i .| awaitForever (processFileC c)
-        (_, True) -> processFileC c i
-        (_, _) -> fail $ "nonexistent file/dir: " <> i
-
-processInputsC :: ConduitT ByteString o M () -> ConduitT FilePath o M ()
-processInputsC c = awaitForever (processInputC c)
-
-processInputs :: Monoid a => ConduitT ByteString a M () -> [FilePath] -> IO a
-processInputs c is = runConduitRes $ yieldMany is .| processInputsC c .| foldC
-
-processGpxC :: ConduitT ByteString InputEvent M ()
-processGpxC = parseBytes def .| parseGpxC
-
-parseGpxC :: ConduitT Event InputEvent M ()
-parseGpxC = do
-    force "<gpx> expected" $ tagIgnoreAttrs (unqual "gpx") $ do
-        void $ many' $ tagIgnoreAttrs (unqual "trk") $ do
-            manyYield' $ tagIgnoreAttrs (unqual "trkseg") $ do
-                manyYield' $ tag' (unqual "trkpt") trkPkAttrs $ \pt -> do
-                    many_ ignoreAnyTreeContent
-                    pure pt
-                pure EndTrkSeg
+parseGpxC :: ConduitT ByteString InputEvent M ()
+parseGpxC = parseBytes def .| parseXmlC
     where
+        parseXmlC = force "<gpx> expected" $ tagIgnoreAttrs (unqual "gpx") $ do
+            void $ many' $ tagIgnoreAttrs (unqual "trk") $ do
+                manyYield' $ tagIgnoreAttrs (unqual "trkseg") $ do
+                    manyYield' $ tag' (unqual "trkpt") trkPkAttrs $ \pt -> do
+                        many_ ignoreAnyTreeContent
+                        pure pt
+                    pure EndTrkSeg
         unqual t = matching ((t ==) . nameLocalName)
         trkPkAttrs = do
             lat <- read . T.unpack <$> requireAttr "lat"
             lon <- read . T.unpack <$> requireAttr "lon"
             ignoreAttrs
             pure Point{ ptLat = lat, ptLon = lon }
+
+parseFileC :: FilePath -> ConduitT i InputEvent M ()
+parseFileC i = sourceFile i .| ungzipIfNeeded .| parseGpxC
+    where
+        ungzipIfNeeded
+            | "gz" `isExtensionOf` i = ungzip
+            | otherwise = awaitForever yield
+
+isGpx :: FilePath -> Bool
+isGpx f = "gpx" `isExtensionOf` f || "gpx.gz" `isExtensionOf` f
+
+gpxInDirectoryC :: FilePath -> ConduitT a FilePath M ()
+gpxInDirectoryC d = sourceDirectoryDeep False d .| filterC isGpx
+
+parseInputC :: FilePath -> ConduitT i InputEvent M ()
+parseInputC i = do
+    isDir <- liftIO $ doesDirectoryExist i
+    isFile <- liftIO $ doesFileExist i
+    case (isDir, isFile) of
+        (True, _) -> gpxInDirectoryC i .| awaitForever parseFileC
+        (_, True) | isGpx i -> parseFileC i
+        (_, _) -> fail $ "bad input file/dir: " <> i
+
+parseInputsC :: ConduitT FilePath InputEvent M ()
+parseInputsC = awaitForever parseInputC
+
+parseInputs :: Monoid a => ConduitT InputEvent a M () -> [FilePath] -> IO a
+parseInputs c is = runConduitRes $ yieldMany is .| parseInputsC .| c .| foldC

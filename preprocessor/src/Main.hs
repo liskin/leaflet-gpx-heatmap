950fe3ea63f652578f640aca50804db4abb5cc95
Tomas Janousek <tomi@nomi.cz>
2019-09-01 18:16:55 +0200

    Show progress report

--- preprocessor/src/Main.hs
+++ preprocessor/src/Main.hs
@@ -1,18 +1,35 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
 
-import Control.Concurrent.ParallelIO.Global (parallel_)
+import Control.Concurrent.ParallelIO.Global (parallel)
+import Control.DeepSeq
 import Data.List (sortBy)
+import Data.Map (Map)
 import Data.Ord (comparing)
+import System.Console.AsciiProgress
 import System.Environment (getArgs)
 
-import qualified Data.Vector.Unboxed as UV
+import qualified Data.Map as Map
 
 import Main.Input
 
 main :: IO ()
 main = do
-    args <- getArgs
-    inputs <- sortBy (comparing fst) <$> expandInputs args
-    parallel_ $ flip map inputs $ \(filename, parseInput) -> do
-        input <- parseInput
-        sum (map UV.length input) `seq` filename `seq` putStrLn "."
+    inputs <- loadInputs =<< getArgs
+    print $ Map.size inputs
+
+loadInputs :: [FilePath] -> IO (Map FilePath [TrackSegment])
+loadInputs files = do
+    inputs <- sortBy (comparing fst) <$> expandInputs files
+    fmap Map.fromList $ progressParallel "Loading GPX files" $
+        flip map inputs $ \(filename, parseInput) -> do
+            !input <- force <$> parseInput
+            pure (filename, input)
+
+progressParallel :: String -> [IO a] -> IO [a]
+progressParallel what as = displayConsoleRegions $ do
+    progress <- newProgressBar def{ pgTotal = fromIntegral (length as), pgFormat = format }
+    parallel $ map (<* tick progress) as
+    where
+        format = what <> " :percent [:bar] :current/:total (for :elapsed, :eta remaining)"

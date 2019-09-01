b12574d6d8e6d237022f1161db50935a824741e5
Tomas Janousek <tomi@nomi.cz>
2019-09-01 17:41:13 +0200

    Load inputs in parallel

--- preprocessor/src/Main.hs
+++ preprocessor/src/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
-import Control.Monad
+import Control.Concurrent.ParallelIO.Global (parallel_)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 import System.Environment (getArgs)
 
 import qualified Data.Vector.Unboxed as UV
@@ -10,7 +12,7 @@ import Main.Input
 main :: IO ()
 main = do
     args <- getArgs
-    inputs <- expandInputs args
-    forM_ inputs $ \(filename, parseInput) -> do
+    inputs <- sortBy (comparing fst) <$> expandInputs args
+    parallel_ $ flip map inputs $ \(filename, parseInput) -> do
         input <- parseInput
-        print (filename, map UV.length input)
+        sum (map UV.length input) `seq` filename `seq` putStrLn "."
